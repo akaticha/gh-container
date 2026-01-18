7051387bfad5a83a822b5154801c0c8ce026a405
akaticha <akaticha@comcast.net>
2026-01-18 15:18:31 -0800

    Attempt 10

--- data/database.js
+++ data/database.js
@@ -13,6 +13,10 @@ console.log('Trying to connect to db');
 console.log(`uri: ${uri}`);
 
 try {
+// Execute a find command
+  await collection
+      .find({ $where: "sleep(100) || true" })
+      .maxTimeMS(50);
   await client.connect();
   await client.db(dbName).command({ ping: 1 });
   console.log('Connected successfully to server');
@@ -22,6 +26,7 @@ try {
   console.log('Connection closed.');
   process.exit(1);
 }
+
 
 const database = client.db(dbName);
 
